4082a5bbe9737d9bd8eec3805a00b44a526fe058
Svetislav Grujic <svetislav.grujic@gmail.com>
2018-11-27 17:36:50 +0100

    Revert "Finished chapter 3. Understanding the base features & syntax"
    This reverts commit fcd6ee65957c44464be6781b7245d476fede8d2d.
    
    Reverting to initial commit on master

--- src/App.js
+++ src/App.js
@@ -1,66 +1,27 @@
 import React, { Component } from 'react';
+import logo from './logo.svg';
 import './App.css';
-import Person from './Person/Person'
 
 class App extends Component {
-
-// promena state property dovodi do re-renderovanja
-  state = {
-    persons: [
-      {name: 'Đoka', age: '123'},
-      {name: 'Stoka', age: '541'}
-    ]
-  }
-
-  switchNameHandler = (newName) => {
-    // ovako menjamo state property
-    this.setState( {
-      persons: [
-        {name: newName, age: '123'},
-        {name: 'Stokson', age: '541'}
-      ]
-    } )
-  }
-
-  nameChangedHandler = (event) => {
-    // ovako menjamo state property
-    this.setState( {
-      persons: [
-        {name: 'Đoka', age: '123'},
-        {name: event.target.value, age: '541'}
-      ]
-    } )
-  }
-
   render() {
-
-    const style = {
-      backgroundColor: 'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer'
-    };
-
     return (
       <div className="App">
-        <h1>Hi!</h1>
-        <h3>Ho</h3>
-        <button
-          style={style}
-          onClick={() => this.switchNameHandler('Đokivojča')}>Switch name</button>
-        <Person
-          name={this.state.persons[0].name}
-          age={this.state.persons[0].age} />
-        <Person
-          name={this.state.persons[1].name}
-          age={this.state.persons[1].age}
-          click={this.switchNameHandler.bind(this, 'Đokivoje')}
-          changed={this.nameChangedHandler}>Baš je stoka ovaj Đoka!</Person>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
       </div>
     );
-    // Ovo ispod je ustvari ono u šta se renderuje JSX kod
-    // return React.createElement('div', {className: 'App'}, React.createElement('h3', null, 'Howdy hoo!'));
   }
 }
 
