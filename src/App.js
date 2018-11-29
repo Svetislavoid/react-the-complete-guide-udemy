abf01cbd461edd740adee03be899e535a390582e
Svetislav Grujic <svetislav.grujic@gmail.com>
2018-11-29 20:37:17 +0100

    Finished chapter 4. Working with lists and conditionals

--- src/App.js
+++ src/App.js
@@ -7,29 +7,39 @@ class App extends Component {
 // promena state property dovodi do re-renderovanja
   state = {
     persons: [
-      {name: 'Đoka', age: '123'},
-      {name: 'Stoka', age: '541'}
-    ]
+      {id: 'sljkdf', name: 'Đoka', age: '123'},
+      {id: 'xcvihi', name: 'Stoka', age: '541'}
+    ],
+    showPersons: false
   }
 
-  switchNameHandler = (newName) => {
-    // ovako menjamo state property
-    this.setState( {
-      persons: [
-        {name: newName, age: '123'},
-        {name: 'Stokson', age: '541'}
-      ]
-    } )
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+
+    const person = {
+      ...this.state.persons[personIndex]
+    };
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState( {persons: persons} )
   }
 
-  nameChangedHandler = (event) => {
-    // ovako menjamo state property
-    this.setState( {
-      persons: [
-        {name: 'Đoka', age: '123'},
-        {name: event.target.value, age: '541'}
-      ]
-    } )
+  deletePersonHandler = (personIndex) => {
+    //const persons = this.state.persons;
+    const persons = [...this.state.persons]; // bolje je raditi ovako, pravimo kopiju niza iz state-a kojom nadalje manipulišemo
+    persons.splice(personIndex,1);
+    this.setState({persons: persons});
+  }
+
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({showPersons: !doesShow});
   }
 
   render() {
@@ -42,21 +52,31 @@ class App extends Component {
       cursor: 'pointer'
     };
 
+    let persons = null;
+
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          {this.state.persons.map((person, index) => {
+            return <Person
+              click = {() => this.deletePersonHandler(index)}
+              name = {person.name}
+              age = {person.age}
+              key = {person.id} // key property je bitan da bi React efikasnije radio re-renderovanje
+              changed = {(event) => this.nameChangedHandler(event, person.id)} />
+          })}
+        </div>
+      );
+    }
+
     return (
       <div className="App">
         <h1>Hi!</h1>
         <h3>Ho</h3>
         <button
           style={style}
-          onClick={() => this.switchNameHandler('Đokivojča')}>Switch name</button>
-        <Person
-          name={this.state.persons[0].name}
-          age={this.state.persons[0].age} />
-        <Person
-          name={this.state.persons[1].name}
-          age={this.state.persons[1].age}
-          click={this.switchNameHandler.bind(this, 'Đokivoje')}
-          changed={this.nameChangedHandler}>Baš je stoka ovaj Đoka!</Person>
+          onClick={this.togglePersonsHandler}>Toggle Persons</button>
+        {persons}
       </div>
     );
     // Ovo ispod je ustvari ono u šta se renderuje JSX kod
