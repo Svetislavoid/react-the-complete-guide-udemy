4b0b4508b387cf582259125ce92d16c4bf1d3efb
Svetislav Grujic <svetislav.grujic@gmail.com>
2018-11-30 01:09:44 +0100

    Finished chapter 5. Styling React components & elements

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import './Person.css';
+import classes from './Person.module.css';
 
 const person = (props) => {
     return (
-        <div className='Person'>
+        <div className={classes.Person}>
             <p onClick={props.click}>I am a person (I think).</p>
             <p>My name is {props.name} and here is a number {props.age}. Here is a random number: {Math.round(Math.random() * 12345)}</p>
             <p>{props.children}</p>
